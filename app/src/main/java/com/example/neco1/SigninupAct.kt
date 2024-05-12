d74de7464dcb38b12400d85ecbd80780f7208fd3
David <Dgagiev@gmail.com>
2024-05-12 21:00:02 +0300

    br4 Переход на второй экран по нажатию кнопки. Рабочий

--- app/src/main/java/com/example/neco1/SigninupAct.kt
+++ app/src/main/java/com/example/neco1/SigninupAct.kt
@@ -1,16 +1,33 @@
 package com.example.neco1
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.neco1.constance.Constance
 import com.example.neco1.databinding.ActSigninupBinding
 
 
 class SigninupAct : AppCompatActivity() {
     lateinit var bindingClass: ActSigninupBinding
+    private var signState = "empty"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActSigninupBinding.inflate(layoutInflater)
 
         setContentView(bindingClass.root)
+
+        signState = intent.getStringExtra(Constance.SIGN_STATE)!!
+        if (signState == Constance.SIGN_IN_STATE) {
+            bindingClass.edName1.visibility = View.GONE
+            bindingClass.edName2.visibility = View.GONE
+            bindingClass.edName3.visibility = View.GONE
+            bindingClass.bAvatar.visibility = View.INVISIBLE
+        }
+    }
+
+    fun onClickDone(view: View) {
+        if (signState == Constance.SIGN_IN_STATE) {
+
+        }
     }
 }
